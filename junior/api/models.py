2731cc2c854b2aa308d0b67b528034f805d8174e
Anton Shcherbak <sherback.anton@gmail.com>
2021-07-25 13:40:02 +0300

    Запись 4.2

--- junior/api/models.py
+++ junior/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class BookCover(models.IntegerChoices):
+    HARD = 1
+    SOFT = 2
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=127)
+    pages = models.PositiveIntegerField()
+    cover = models.PositiveSmallIntegerField(choices=BookCover.choices)
+    authors = models.ManyToManyField(Author)
